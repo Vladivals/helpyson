103bd715ffba99e60cb88320ea21c9d14e9f2964
vladpochobut <vladpochobut1@mail.ru>
2023-06-14 14:39:37 +0200

    hel_3,4,5
    changed :
    the submit button appears only after entering >= 1 character. Slightly changed the size of the chat. Changed the chat request input field to expand upwards when typing a large message.

--- Assets/Scripts/UI/ChatBehaviour.cs
+++ Assets/Scripts/UI/ChatBehaviour.cs
@@ -13,7 +13,37 @@ public class ChatBehaviour : MonoBehaviour
 
     private List<RaycastResult> raycastResults;
 
+    public float minHeight = 30f; // Минимальная высота поля ввода
+    public float maxHeight = 200f; // Максимальная высота поля ввода
+    public float extraHeight = 5f; // Дополнительная высота для предотвращения обрезания текста
+    private RectTransform _inputFieldRectTransform;
+    private TMPro.TMP_Text _inputFieldText;
+
+    private void OnEnable()
+    {
+        inputField.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    void Start()
+    {
+        _inputFieldRectTransform = inputField.GetComponent<RectTransform>();
+        _inputFieldText = inputField.textComponent;
+    }
+
     private void Update()
+    {
+        OpenCloseMessageArea();
+        // Установите минимальную и максимальную высоту поля ввода
+        SetMaxMinHeight();
+    }
+
+    private void SetMaxMinHeight()
+    {
+        _inputFieldRectTransform.sizeDelta = new Vector2(_inputFieldRectTransform.sizeDelta.x,
+                    Mathf.Clamp(_inputFieldText.preferredHeight + extraHeight, minHeight, maxHeight));
+    }
+
+    private void OpenCloseMessageArea()
     {
         var currentSelectedObject = EventSystem.current.currentSelectedGameObject;
         messageArea.SetActive(
@@ -22,5 +52,10 @@ public class ChatBehaviour : MonoBehaviour
             || currentSelectedObject == messageArea.gameObject
         );
         messageArea.transform.DOScale(Convert.ToInt32(messageArea.activeSelf), 1);
+    }
+
+    public void OnValueChanged(string value)
+    {
+        button.gameObject.active = value.Length >= 1 ? true : false;
     }
 }
